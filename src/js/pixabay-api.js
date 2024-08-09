26b697bbfc0f5a1592e73c36a3508cdf06b280d8
Valeri <mylenovo21072017@gmail.com>
2024-08-09 23:01:48 +0300

    add all

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,2 +1,20 @@
-'use strict';
+const API_KEY = '45360350-58fd54754bd8f677bef7c2cc3';
+const BASE_URL = 'https://pixabay.com/api/';
 
+
+export function searchImages(query) {
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+    });
+
+    return fetch(`${BASE_URL}?${searchParams}`).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+}
